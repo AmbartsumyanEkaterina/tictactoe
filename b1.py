9f2d06f226858cb900a39b7a80823a0b7ed0e582
AmbartsumyanEkaterina <66012748+AmbartsumyanEkaterina@users.noreply.github.com>
2020-06-02 12:21:10 +0300

    Add files via upload

--- b1.py
+++ b1.py
@@ -1,13 +1,24 @@
 import telebot;
 bot = telebot.TeleBot('1101157557:AAEbuY7qdr9xHROQOEuAC5hZ3UyWWuz1S7U');
-
+from telebot import types
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
+    
     if message.text == "Привет":
 
-        bot.send_message(message.from_user.id, "Привет, поиграем в Крестики-Нолики?")
+        bot.send_message(message.from_user.id, "Привет!")
+        keyboard = types.InlineKeyboardMarkup()
+    
+        key_yes = types.InlineKeyboardButton(text='Да', callback_data='answer')
+
+        keyboard.add(key_yes)
+
+        key_no = types.InlineKeyboardButton(text='Нет', callback_data='answer')
+
+        keyboard.add(key_no)
+        bot.send_message(message.from_user.id, text='Поиграем в Крестики-Нолики?', reply_markup=keyboard) 
+
 
     elif message.text == "/help":
 
